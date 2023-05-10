9f683760c81f987c92486f9a0583d1420313a6e8
romeeeoo <tarassenko@me.com>
2023-05-10 18:24:13 +0300

    убарл лишнии поля для общей модели юзера

--- accounts/models/custom_user.py
+++ accounts/models/custom_user.py
@@ -7,16 +7,9 @@ from accounts.managers import CustomUserManager
 
 class CustomUser(AbstractUser):
     username = None
-    email = models.EmailField(unique=True, blank=False, max_length=320),
+    email = models.EmailField(unique=True, blank=False, max_length=320)
     avatar = models.ImageField(upload_to="uploads/avatar", blank=True)
-    birthdate = models.DateField(blank=False)
-    current_location = models.CharField(choices=LocationEnum.choices, max_length=100, blank=False),
-    is_guide = models.BooleanField(default=False),
-    is_tourist = models.BooleanField(default=False),
-    is_email_confirmed = models.BooleanField(default=False)
-
     objects = CustomUserManager()
-
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
