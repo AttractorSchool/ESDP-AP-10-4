c08795563af71951664a13604d33801b7319cc7e
romeeeoo <tarassenko@me.com>
2023-05-16 22:35:55 +0300

    #3 чистый код согласно flake8

--- accounts/views/__init__.py
+++ accounts/views/__init__.py
@@ -1,6 +1,6 @@
 from .user_login_view import LoginView, logout_view
-from .user_register_view import RegisterView
 from .update_profile import UpdateGuideProfile
+from .user_register_view import RegisterView
 
 
 __all__ = ['RegisterView', 'LoginView', 'logout_view', 'UpdateGuideProfile']
