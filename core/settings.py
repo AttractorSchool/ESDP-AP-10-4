6776d04c3d914bc29941d5131031dcce0e3cb6fe
Uralbek Yergenov <oralbekergenov@gmail.com>
2023-05-18 22:34:37 +0600

    Чистый код по flake8

--- core/settings.py
+++ core/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': env.db_url('DATABASE_URL')
+    'default': env.db_url('DATABASE_URL'),
 }
 
 
