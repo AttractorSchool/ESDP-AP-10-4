af259d393e1ceb6e6122a1d1f517ec0853c3fc69
Aigerim Avezbaeva <av21m1999@gmail.com>
2023-05-26 21:57:59 +0600

    #18 исправление код ревью

--- accounts/views/index_profile.py
+++ accounts/views/index_profile.py
@@ -1,14 +1,17 @@
+from django.shortcuts import get_object_or_404
+
 from choices import StatusChoice
-from django.contrib.auth import get_user_model
 from django.views.generic import DetailView
+
+from accounts.models import Profile
 
 
 class GuideProfileView(DetailView):
-    model = get_user_model()
+    model = Profile
     template_name = 'guide_profile.html'
     context_object_name = 'user_obj'
 
     def get_queryset(self):
         queryset = super().get_queryset()
-        queryset = queryset.filter(profile__verification_status=StatusChoice.CONFIRMED)
+        queryset = queryset.filter(verification_status=StatusChoice.CONFIRMED)
         return queryset
