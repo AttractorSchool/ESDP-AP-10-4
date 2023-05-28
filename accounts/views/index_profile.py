5ef96cf18549703e9a5cda215ec2e5dfd43d2a8d
Aigerim Avezbaeva <av21m1999@gmail.com>
2023-05-29 02:02:10 +0600

    #18 добавлены изменения согласно код ревью

--- accounts/views/index_profile.py
+++ accounts/views/index_profile.py
@@ -1,15 +1,27 @@
 from choices import StatusChoice
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.http import Http404
 from django.views.generic import DetailView
 
 from accounts.models import Profile
 
 
-class GuideProfileView(DetailView):
+class GuideProfileView(UserPassesTestMixin, DetailView):
     model = Profile
     template_name = 'guide_profile.html'
     context_object_name = 'user_obj'
 
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        queryset = queryset.filter(verification_status=StatusChoice.CONFIRMED)
-        return queryset
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        verification_status = self.object.verification_status
+        context['is_verified'] = verification_status == StatusChoice.CONFIRMED
+        return context
+
+    def test_func(self):
+        if self.request.user.is_authenticated:
+            obj = self.get_object()
+            if self.request.user == obj.user or self.request.user.is_superuser:
+                return True
+        if obj.verification_status == StatusChoice.CONFIRMED:
+            return True
+        raise Http404('Пользователь не найден')
