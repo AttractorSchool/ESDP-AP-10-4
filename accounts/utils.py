0570d5aa1965ce8cca33bb828186105d25df8d10
romeeeoo <tarassenko@me.com>
2023-06-02 18:03:48 +0300

    #41 исправлен баг с при обновлении роли пользователя

--- accounts/utils.py
+++ accounts/utils.py
@@ -5,7 +5,7 @@ def create_profile(form):
     user = form.save()
     user.set_password(form.cleaned_data.get('password'))
     if form.cleaned_data.get("is_guide"):
-        GuideProfile.objects.create(user=user)
+        GuideProfile.objects.get_or_create(user=user)
         user.guide_profile.save()
         return user
     return user
