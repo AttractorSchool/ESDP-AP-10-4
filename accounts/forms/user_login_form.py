d55cbfb558dc30ec05889e50ebb25ea55fbc9b43
Ilkham Baudinov <ilkhambaudinov@gmail.com>
2023-06-02 17:42:11 +0600

    #31 Привести в порядок шаблоны 'регистрации и логина'

--- accounts/forms/user_login_form.py
+++ accounts/forms/user_login_form.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class UserLoginForm(forms.Form):
-    email = forms.EmailField(required=True, label='E-mail')
+    email = forms.EmailField(required=True)
     password = forms.CharField(required=True, label='Password', widget=forms.PasswordInput)
