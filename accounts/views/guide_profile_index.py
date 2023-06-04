305d274636260beaef32e0313a4f8e64c3277ec0
romeeeoo <tarassenko@me.com>
2023-06-04 16:26:54 +0300

    #53 Корректное отображения профиля для разных ролей
    в guide_profile.html:
    -  добавлены проверки с новый логикой по типу юзера
    - включены туры списком
    в guide_profile_index.py:
    - добавлены переменные guide_tours и guide_confirmed_tours в contex представления GuideProfileView
    - Переписан test_func так чтобы анонимный юзер мог просматривать профили гидов

--- accounts/views/guide_profile_index.py
+++ accounts/views/guide_profile_index.py
@@ -3,6 +3,7 @@ from django.contrib.auth.mixins import UserPassesTestMixin
 from django.http import Http404
 from django.views.generic import DetailView
 
+
 from accounts.models import GuideProfile
 
 
@@ -15,13 +16,16 @@ class GuideProfileView(UserPassesTestMixin, DetailView):
         context = super().get_context_data(**kwargs)
         verification_status = self.object.verification_status
         context['is_verified'] = verification_status == StatusChoice.CONFIRMED
+        context['guide_tours'] = self.object.user.tour.all()
+        context['guide_confirmed_tours'] = self.object.user.tour.filter(moderation_status=StatusChoice.CONFIRMED)
         return context
 
+
+
     def test_func(self):
-        if self.request.user.is_authenticated:
-            obj = self.get_object()
-            if self.request.user == obj.user or self.request.user.is_superuser:
-                return True
-            elif obj.verification_status == StatusChoice.CONFIRMED:
-                return True
+        obj = self.get_object()
+        if self.request.user == obj.user or self.request.user.is_superuser:
+            return True
+        elif obj.verification_status == StatusChoice.CONFIRMED:
+            return True
         raise Http404('Пользователь не найден')
