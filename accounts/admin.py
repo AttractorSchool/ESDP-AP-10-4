47aa2a676364db19e62aa4d5655bb4d33ae4235c
Ilkham Baudinov <ilkhambaudinov@gmail.com>
2023-06-06 00:49:14 +0600

    Поправил Редактирование данных Юзера в Админке

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,16 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
 from .models import User, GuideProfile
 from .utils import create_user, create_profile
 
 
-
 # Register your models here.
 @admin.register(User)
 class CustomAdmin(admin.ModelAdmin):
-    exclude = ('is_guide', 'is_tourist')
+    exclude = ('is_guide', 'is_tourist', 'password')
 
     def save_model(self, request, obj, form, change):
+        if get_user_model().objects.filter(pk=obj.pk):
+            return obj.save()
         user = create_user(form)
         create_profile(user)
         super().save_model(request, obj, form, change)
@@ -27,4 +29,3 @@ class GuideProfileAdmin(admin.ModelAdmin):
     class Meta:
         verbose_name = 'Профиль'
         verbose_name_plural = 'Профили'
-
