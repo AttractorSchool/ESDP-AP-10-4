0c6e2af222938f1b5211c6e5b5bf5b7fa3154336
Aigerim Avezbaeva <av21m1999@gmail.com>
2023-06-08 23:51:37 +0600

    #18 Добавлено отображение забронированных туров в лк Туриста

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import LoginView, logout_view
 from .views import RegisterView
 from .views.guide_profile_index import GuideProfileView
+from .views.tourist_profile_view import TouristDetailView
 from .views.update_guide import UpdateGuide
 
 
@@ -11,6 +12,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', logout_view, name='logout'),
     path('guide-profile/<int:pk>', GuideProfileView.as_view(), name='guide_profile'),
-    # path('tourist-profile/<int:pk>', TouristDetailView.as_view(), name='tourist-profile'),
+    path('tourist-profile/<int:pk>', TouristDetailView.as_view(), name='tourist-profile'),
     path('update/<int:pk>', UpdateGuide.as_view(), name='update_guide'),
 ]
--- accounts/views/tourist_profile_view.py
+++ accounts/views/tourist_profile_view.py
@@ -2,11 +2,21 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.views.generic import DetailView
 
+from tours.models import Tour
+
 
 class TouristDetailView(UserPassesTestMixin, DetailView):
     model = get_user_model()
     template_name = 'account/tourist_profile.html'
     context_object_name = 'user_obj'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        tourist_tour_ids = self.object.tours.values_list('tour_id', flat=True)
+        tourist_tours = Tour.objects.filter(id__in=tourist_tour_ids)
+        context['tourist_tours'] = tourist_tours
+        return context
+
     def test_func(self):
-        return self.request.user.is_authenticated
+        return True
+
