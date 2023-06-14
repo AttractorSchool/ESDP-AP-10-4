b966180e78cd42b5aa489308207b1b8f46037142
Ruslan Zulufov <gothiq2302@gmail.com>
2023-06-14 22:08:48 +0600

    #67 Flake8

--- tours/models/rating.py
+++ tours/models/rating.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-from django.db.models import UniqueConstraint
 
 
 class TourRating(models.Model):
@@ -39,4 +38,4 @@ class TourRating(models.Model):
         verbose_name_plural = 'Рейтинги'
 
     def __str__(self):
-        return f"{self.tour}: {self.professionalism}, {self.correspondence}"
+        return f'{self.tour}: {self.professionalism}, {self.correspondence}'
