cde1ffd7db31e0e233db1a9d730eedf5d74f40ca
Uralbek Yergenov <oralbekergenov@gmail.com>
2023-06-22 17:54:37 +0500

    #81 Создать модель Пассажира

--- booking/models.py
+++ booking/models.py
@@ -20,3 +20,31 @@ class Booking(models.Model):
 
     def __str__(self):
         return f'{self.user} | {self.tour}'
+
+
+class Passenger(models.Model):
+    first_name = models.CharField(
+        max_length=256,
+        blank=False,
+        null=False,
+        verbose_name='Имя пассажира',
+    )
+
+    last_name = models.CharField(
+        max_length=256,
+        blank=False,
+        null=False,
+        verbose_name='Фамилия пассажира',
+    )
+
+    birthdate = models.DateField(null=False, blank=False)
+
+    booking = models.ForeignKey(
+        to='booking.Booking',
+        related_name='passengers',
+        on_delete=models.CASCADE,
+        verbose_name='Бронирование',
+    )
+
+    def __str__(self):
+        return f'{self.booking} {self.first_name} {self.last_name} {self.birthdate}'
